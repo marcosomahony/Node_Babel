4b3573e0d1e46ab80126443893830e34ab5fc2f2
Marcos O'Mahony <marcos.omahony.alonso@alumnos.upm.es>
2018-03-19 11:53:19 +0100

    Recreo

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const modulo = require('./modulo'); 
-console.log(modulo.suma(7,3));
+const P = require('bluebird');
+
+function suma(a, b) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 20);
+    });
+}
+
+const array = [[2,3], [4,5]];
+const promesas = [];
+array.forEach((par) => promesas.push(suma(par[0], par[1])));
+
+console.log(promesas);
+
+P.map(promesas, (result) => result * 20)
+.then(console.log);
+
+/* Promise.all(promesas)
+.then((result) => {
+    console.log(result);
+}); */
